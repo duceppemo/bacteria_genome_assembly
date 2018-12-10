ebc727c2b39f02730cdc18848d5f44bade6f62ea
Marc-Olivier Duceppe <marc-olivier.duceppe@inspection.gc.ca>
2018-12-10 16:58:45 -0500

    Added "Most Informatice Hit" algorithm
    And added/modified few methods to get it to a workable script.

--- bda.py
+++ bda.py
@@ -1,11 +1,13 @@
 #!/usr/local/env python3
 
-__author__ = 'duceppemo'
+__author__ = 'duceppemo, OldAdam70'
 __version__ = '0.1'
 
 
 import os
 from nested_dict import nested_dict
+from Bio import SeqIO
+import os.path
 
 
 class BDA(object):
@@ -15,26 +17,64 @@ class BDA(object):
 
         self.args = args
         self.input = args.input
-        self.out = args.output
+        self.output = args.output
+
+        # Check input file
+        self.check_input_file(self.input)
+
+        # Check output file
+        self.check_output_file(self.output)
 
         # Number of cpu
         self.cpus = int(multiprocessing.cpu_count())
 
+        # Empty ata structures
         self.blast_dict = nested_dict()
+        self.contigs_dict = dict()
 
-        # Default output path is input assembly path
-        if not self.out:
-            self.out = os.path.dirname(self.input) + "/"
-
-        self.position_list = list()  # contain tuples (contig, position, sense
+        # List of allowed file extensions
+        self.allowed_ext = ['.fas', '.fasta', '.fa']
 
         # run the script
         self.run()
 
     def run(self):
         self.check_dependencies()
+        self.check_input_file(self.input)
         self.parse_fasta(self.input)
-        self.run_blastn('nt', self.input)
+        blast_handle = self.run_blastn('nr', self.input)
+        self.parse_blast_output(blast_handle)
+
+    def check_input_file(self, input_file):
+        # Check if input file exists
+        if not os.path.exists(input_file):
+            raise Exception('Input file "{}" does not exist'.format(input_file))
+
+        # Check if input file is a file
+        if not os.path.isfile(input_file):
+            raise Exception('Provided input "{}" is not a file'.format(input_file))
+
+        # Check if input is a fasta file
+        self.is_fasta(input_file)
+
+    def check_output_file(self, output_file):
+        # Check if output file was provided
+        if self.output:
+            # Check if output path exists, if not create it
+            out_folder = os.path.dirname(output_file)
+            if not os.path.exists(out_folder):
+                os.makedirs(out_folder)
+        else:
+            # Output file is equal to input file with "_bda" append
+            input_path = os.path.dirname(self.input)
+            filename, input_ext = os.path.splitext(os.path.basename(self.input))
+            self.output = input_path + '/' + filename + '_dba' + input_ext
+
+    def is_fasta(self, fasta):
+        with open(fasta) as f:
+            first_line = f.readline()
+        if first_line[:1] != '>':
+            raise Exception("Input file {} is not valid fasta file")
 
     def parse_fasta(self, f):
         """
@@ -42,38 +82,39 @@ class BDA(object):
         :param f: Assembly file in fasta format
         :return: Populated dictionary
         """
-        from Bio import SeqIO
 
         self.contigs_dict = SeqIO.to_dict(SeqIO.parse(f, format='fasta'))
 
     def run_blastn(self, ref_db, query):
         """
         Perform blastn using biopython
-        :param ref: A fasta file for which "makeblastdb' has already been run
-        :param query: The assembly, in fasta format
-        :return: XML file
+        :param ref_db: A fasta file for which "makeblastdb' was already run
+        :param query: Protein fasta file
+        :return: blast handle
         """
-        from Bio.Blast.Applications import NcbiblastnCommandline
+
+        from Bio.Blast.Applications import NcbiblastpCommandline
         from io import StringIO
 
-        blastn = NcbiblastnCommandline(db=ref_db, query=query, evalue='1e-10',
-                                             outfmt=5, max_target_seqs=5,
-                                             num_threads=self.cpus)
-        (stdout, stderr) = blastn()
+        print("Running blastp...")
+
+        ref_db = '/media/30tb_raid10/db/nr/nr'
+        blastx = NcbiblastpCommandline(db=ref_db, query=query, evalue='1e-10',
+                                       outfmt=5, max_target_seqs=20,
+                                       num_threads=self.cpus)
+        (stdout, stderr) = blastx()
 
         if stderr:
-            raise Exception('There was a problem with the blast')
+            raise Exception('There was a problem with the blast:\n{}'.format(stderr))
 
         # Search stdout for matches - if the term Hsp appears (the .find function will NOT
         # return -1), a match has been found, and stdout is written to file
         if stdout.find('Hsp') != -1:
             blast_handle = StringIO(stdout)  # Convert string to IO object
-            self.parse_blast_output(blast_handle)
         else:
-            print("No reference sequences found.")
+            raise Exception("No blast results found.")
 
-        # TODO -> Check that no contigs have both the dnaA and the repA genes
-        # Raise a warning to the user if is the case
+        return blast_handle
 
     def parse_blast_output(self, blast_handle):
         """
@@ -82,34 +123,91 @@ class BDA(object):
         :return:
         """
         from Bio import SearchIO
-        from Bio.Seq import Seq
+
+        # Create the list of words to filter out uninformative hits
+        filter_strings = ['putative', 'protein like', 'protein related', 'contains similarity to',
+                          'predicted', 'hypothetical protein', 'unnamed protein product',
+                          'unknown', 'expressed protein', 'uncharacterized', 'probable',
+                          'possible', 'potential']
 
         records_dict = SearchIO.to_dict(SearchIO.parse(blast_handle, 'blast-xml'))
-        fixed_seq = None
 
-        for id, qresult in records_dict.items():  # this is a sorted dictionare, with best hit first?
-            for hit in qresult.hits:
-                for hsps in hit.hsps:
-                    query_seq = self.contigs_dict[id].seq
+        # Open output file handle in write mode
+        with open(self.output, 'w') as f:
+            for id, qresult in records_dict.items():
+                hit_descriptions = []
+                e_values = []
+                bit_scores = []
+                proper_desc = 'hypothetical protein'
 
-                    """
-                    https://groups.google.com/forum/#!topic/blast2go/B6g6PfX6YW8
-                    
-                    Removed words are: putative, protein like,
-                    protein related, contains similarity to, similar to predicted
-                    
-                    Removed descriptions are:  hypothetical protein, unnamed protein
-                    product, unknown, expressed protein
-                    """
+                for hit in qresult.hits:
+                    hit_desc = hit.description
 
-                    filter_words = ['putative', 'hypothetical']
+                    # Filter out uninformative hits here, create a list containing the remaining hits
+                    # If there are hits left after the filtering, find the MIH
+                    # If no hits are left after filtering, just return "hypothetical protein"
+                    if not any([filter in str(hit_desc).lower() for filter in filter_strings]):
+                        hit_descriptions.append(hit_desc)
+                        e_values.append(hit.hsps[0].evalue)
+                        bit_scores.append(hit.hsps[0].bitscore)
 
-                    # TODO -> put good info in object to be filtered
+                if len(hit_descriptions) > 0:
+                    proper_desc = self.identify_mih(hit_descriptions, bit_scores)
+
+                    # Update description according to "find_best_description"
+                    self.contigs_dict[id].description = proper_desc
+
+                # Write to output file
+                f.write('>{} {}\n{}\n'.format(id, proper_desc, self.contigs_dict[id].seq))
 
         # Close file StringIO handle
         blast_handle.close()
 
-        return fixed_seq
+    def identify_mih(self, hits_list, bit_scores):
+        """
+        Identify most informative hit (mih)
+        :param hits_list: a list containing the descriptors of hits for the current qresult
+        :param bit_scores: a list of bit scores associated with the hits from the qresult
+        :return: the most informative hit (MIH) descriptor
+        """
+        from nltk.corpus import stopwords
+        from sklearn.feature_extraction.text import CountVectorizer
+        from sklearn.cluster import KMeans
+        import numpy as np
+
+        # Get the informative part of the hit description first by removing stopwords (common words)
+        filtered_list = []
+        for hit in hits_list:
+            words_list = hit.lower().split()
+            filtered_words = [word for word in words_list if word not in stopwords.words('english')]
+            descriptor = " ".join(filtered_words).split("os=", 1)[0]
+            filtered_list.append(descriptor)
+
+        # Convert the description into a vector which can be used for clustering
+        vectorizer = CountVectorizer()
+        vectorized_hits = vectorizer.fit_transform(filtered_list)
+        vector_array = np.array(vectorized_hits.toarray())
+
+        # Create cluster centers, then test descriptions for which cluster they fall into
+        if len(hits_list) < 8:
+            kmeans = KMeans(n_clusters=len(hits_list), n_init=100).fit(vector_array)
+        else:
+            kmeans = KMeans(n_init=100).fit(vector_array)
+
+        cluster_associations = kmeans.predict(vector_array)
+        # Associate the bit scores with the cluster predictions
+        cluster_aggregates = [(cluster, score) for cluster, score in zip(cluster_associations, bit_scores)]
+        cluster_scores = [0] * 8
+
+        # Determine which clustering contains the highest score
+        # as a combination of number of bit scores and their values
+        for cluster, score in cluster_aggregates:
+            cluster_scores[cluster] += score
+
+        # Find out what descriptions the highest scored cluster is associated with, and return the description
+        MIH_index = list(cluster_associations).index(cluster_scores.index(max(cluster_scores)))
+
+        return hits_list[MIH_index]
 
     def check_dependencies(self):
         pass
@@ -124,8 +222,9 @@ if __name__ == '__main__':
                         required=True,
                         help='Input fasta file to blast')
     parser.add_argument('-o', '--output', metavar='output_bda.fasta',
-                        required=True,
-                        help='Annotated fasta file with useful descriptions')
+                        required=False,
+                        help='Annotated fasta file with useful descriptions'
+                             'Input file name appended with "_bda" will by used if no output file specified')
 
     # Get the arguments into an object
     arguments = parser.parse_args()
